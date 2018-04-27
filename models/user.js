41172d8561faff8289950f23a68f57c91257795e
James <jward1701d@gmail.com>
2018-04-27 16:44:59 -0400

    code for second video in the Nodejs and Passport Login system from Traversy Media YouTube channel. Registration page and registering.

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+mongoose.connect('mongodb://localhost/loginapp');
+
+const db = mongoose.connection;
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    index: true
+  },
+  password: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  name: {
+    type: String
+  }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.createUser = (newUser, cb) =>{
+  bcrypt.genSalt(10, (err, salt)=>{
+    bcrypt.hash(newUser.password, salt, (err, hash)=>{
+      newUser.password = hash;
+      newUser.save(cb);
+    });
+  });
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,56 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user');
+
 // Register
 router.get('/register', (req,res)=>{
   res.render('register');
+});
+
+//Register User
+router.post('/register', (req,res)=>{
+  const regUser = {
+    "name": req.body.name,
+    "email": req.body.email,
+    "username": req.body.username,
+    "password": req.body.password,
+    "password2": req.body.password2
+  };
+
+  // Validation
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Confirm Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  const errors = req.validationErrors();
+
+  if(errors){
+    //console.log(errors)
+    res.render('register', {
+      errors:errors
+    });
+  }else{
+    const newUser = new User({
+      name: regUser.name,
+      email: regUser.email,
+      username: regUser.username,
+      password: regUser.password
+    });
+
+    User.createUser(newUser, (err, user)=>{
+      if(err) throw err;
+      console.log(user);
+    });
+    req.flash('success_msg', "You are registered and can now login");
+    res.redirect('/users/login')
+  }
+
+
 });
 
 // Login
